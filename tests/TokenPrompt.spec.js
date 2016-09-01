f33d5ea0ead6149dc085c31a9c380b882259088e
Funmi Ojo <oluwafunmi.ojo@gmail.com>
2016-09-01 18:48:07 -0400

    Initial commit

---
+++ tests/TokenPrompt.spec.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {mount, shallow} from 'enzyme';
+import {expect} from 'chai';
+
+import TokenPrompt from '../src/components/TokenPrompt';
+
+describe('<Email>', function() {
+	it('should have a dropdown menu to choose token', function() {
+		
+	});
+	
+});
