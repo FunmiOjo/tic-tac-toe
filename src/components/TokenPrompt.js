7cc3ec41b3196bb8ce7a65abf1e388e361c038ff
Funmi Ojo <oluwafunmi.ojo@gmail.com>
2016-09-01 13:03:48 -0400

    Sets up TokenPrompt component

---
+++ src/components/TokenPrompt.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './styles/app.css';
+
+
+class TokenPrompt extends React.Component {
+	constructor(props) {
+		super(props);
+	}
+	
+	render() {
+		return (
+			<div>
+				<h2>Choose your token</h2>
+				<form>
+					<input type='text'>
+					<input type='submit'>
+				</form>
+			</div>
+		);
+	}
+}
+
+export default TokenPrompt;
